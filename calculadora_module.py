b858392043979d09ad05f302e1d0082ff8d2e965
IsacSeixas <142606160+IsacSeixas@users.noreply.github.com>
2023-08-18 11:24:06 -0300

    Create calculadora_module.py

---
+++ calculadora_module.py
@@ -0,0 +1,12 @@
+class Calculadora:
+    def somar(self, a, b):
+        return a + b
+    
+    def sub(self, a, b):
+        return a - b
+    
+    def multiplicacao(self, a, b):
+        return a * b
+    
+    def divi(self, a, b):
+        return a / b
